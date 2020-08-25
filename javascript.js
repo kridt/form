0534bf5aa59a631beb60580bf9eb3236324a676d
Christian <chrnielsen2003@gmail.com>
2020-08-25 12:28:09 +0200

    v2

--- javascript.js
+++ javascript.js
@@ -4,76 +4,67 @@ let statusPassword = document.querySelector(".statusMessage.password")
 let statusFornavn = document.querySelector(".statusMessage.fornavn")
 let statusEfternavn = document.querySelector(".statusMessage.efternavn")
 let statusAdresse = document.querySelector(".statusMessage.adresse")
-let statusAdresseNr = document.querySelector(".statusMessage.adresseNr")
-let statusEtage = document.querySelector(".statusMessage.etage")
 let statusPostnummer = document.querySelector(".statusMessage.postnummer")
 let statusBy = document.querySelector(".statusMessage.by")
 let statusEmail = document.querySelector(".statusMessage.email")
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     let status = true;
 
-statusUsername.innerHTML = "";
-statusPassword.innerHTML = "";
-statusEmail.innerHTML = "";
-statusEtage.innerHTML = "";
-statusBy.innerHTML = "";
-statusAdresseNr.innerHTML = "";
-statusAdresse.innerHTML = "";
-statusPostnummer.innerHTML = "";
-statusFornavn.innerHTML = "";
-statusEfternavn.innerHTML = "";
+    statusUsername.innerHTML = "";
+    statusPassword.innerHTML = "";
+    statusEmail.innerHTML = "";
+    statusBy.innerHTML = "";
+    statusAdresse.innerHTML = "";
+    statusPostnummer.innerHTML = "";
+    statusFornavn.innerHTML = "";
+    statusEfternavn.innerHTML = "";
 
-        if(form.username.value == "") {
-            statusUsername.innerHTML ="du skal vælge et brugernavn"
-            status = false;
-        }
+    if (form.username.value == "") {
+        statusUsername.innerHTML = "du skal vælge et brugernavn"
+        status = false;
+    }
 
-        if(form.password.value == "") {
-            statusPassword.innerHTML ="du skal skrive en adgangskode"
-            status = false;
-        }
-        if(form.fornavn.value == ""){
-            statusFornavn.innerHTML= "<- Skriv dit navn her"
-            status = false;
-        }
-        if(form.efternavn.value == ""){
-            statusEfternavn.innerHTML= "<- Skriv dit efternavn her"
-            status = false;
-        }
-        if(form.adresse.value == ""){
-            statusAdresse.innerHTML= "<- Skriv din adesse her"
-            status = false;
-        }
-        if(form.adresseNr.value == ""){
-            statusAdresseNr.innerHTML= "<- Skriv dit vej/gade nummer her"
-            status = false;
-        }
-        if(form.etage.value == ""){
-            statusEtage.innerHTML= "<- Skriv din etage her"
-            status = false;
-        }
-        if(form.postnummer.value == ""){
-            statusPostnummer.innerHTML= "<- Skriv dit postnummer her"
-            status = false;
-        }
+    if (form.password.value == "") {
+        statusPassword.innerHTML = "du skal skrive en adgangskode"
+        status = false;
+    }
+    if (form.fornavn.value == "") {
+        statusFornavn.innerHTML = "Skriv dit navn her"
+        status = false;
+    }
+    if (form.efternavn.value == "") {
+        statusEfternavn.innerHTML = "Skriv dit efternavn her"
+        status = false;
+    }
+    if (form.adresse.value == "") {
+        statusAdresse.innerHTML = "Skriv din adesse her"
+        status = false;
+    }
 
-        if(form.by.value == ""){
-            statusBy.innerHTML= "<- Skriv by her"
-            status = false;
-        }
-        if(form.email.value == ""){
-            statusEmail.innerHTML= "<- Skriv din E-mail her"
-            status = false;
-        }
+    if (form.postnummer.value == "") {
+        statusPostnummer.innerHTML = "Skriv dit postnummer her"
+        status = false;
+    }
+
+    if (form.by.value == "") {
+        statusBy.innerHTML = "Skriv by her"
+        status = false;
+    }
+    if (form.email.value == "") {
+        statusEmail.innerHTML = "Skriv din E-mail her"
+        status = false;
+    }
 
 
-        if (status == false){
+    if (status == false) {
         return
     }
 
     fetch("login.php")
 
 });
+
+
